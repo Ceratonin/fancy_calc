562a36fbf7e42ca1be34f67e948e3e63e6a05767
Ceratonin <alkash411@gmail.com>
2022-10-08 22:47:24 +0500

    simple calc

--- src/components/Display/Display.tsx
+++ src/components/Display/Display.tsx
@@ -1,17 +1,62 @@
-import { useState, ChangeEvent } from "react";
+import { useState, ChangeEvent, KeyboardEvent, useEffect } from "react";
 
 const Display = () => {
   const [val, setVal] = useState("");
 
-  const handleSubmit = (e: any) => {
-    if (!/[0-9*+-/,.]/.test(e.key)) {
+  const letterCheck = (e: KeyboardEvent<HTMLInputElement>) => {
+    const { key } = e;
+    const regexp = /[(-9√]/g;
+
+    if (
+      ((e.metaKey || e.ctrlKey) &&
+        (key === "v" || key === "c" || key === "x")) ||
+      key === "ArrowLeft" ||
+      key === "ArrowRight" ||
+      key === "ArrowUp" ||
+      key === "ArrowDown" ||
+      key === "Backspace" ||
+      key === "Delete" ||
+      key === " "
+    )
+      return;
+
+    if (!regexp.test(e.key)) {
       e.preventDefault();
-    } else {
-      setVal(e.target.value);
     }
   };
 
-  return <input type="text" className="display" onKeyPress={handleSubmit} />;
+  const handleChange = (e: ChangeEvent<HTMLInputElement>) => {
+    setVal(e.target.value);
+  };
+
+  const calculation = (value: string) => {
+    // eslint-disable-next-line no-eval
+    return eval(value);
+  };
+
+  useEffect(() => {
+    const keyDownHandler = (event: { key: string }) => {
+      if (event.key === "Enter") {
+        setVal(calculation(val));
+      }
+    };
+
+    document.addEventListener("keydown", keyDownHandler);
+
+    return () => {
+      document.removeEventListener("keydown", keyDownHandler);
+    };
+  }, [val]);
+
+  return (
+    <input
+      type="text"
+      className="display"
+      onKeyDown={letterCheck}
+      onChange={handleChange}
+      value={val}
+    />
+  );
 };
 
 export default Display;
